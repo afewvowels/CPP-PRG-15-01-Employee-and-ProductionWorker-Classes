384af58f739f298890f76ff5312415d3e13ad48c
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-20 11:48:01 -0500

    Initial Commit

---
+++ CPP-PRG-15-01-Employee-and-ProductionWorker-Classes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP-PRG-15-01-Employee-and-ProductionWorker-Classes
+//
+//  Created by Keith Smith on 11/20/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
