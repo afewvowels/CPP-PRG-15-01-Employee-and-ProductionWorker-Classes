875570774184f6d915c25a64c7cfa991f9eb3547
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-20 11:51:14 -0500

    Prompt written.

--- CPP-PRG-15-01-Employee-and-ProductionWorker-Classes/main.cpp
+++ CPP-PRG-15-01-Employee-and-ProductionWorker-Classes/main.cpp
@@ -5,11 +5,31 @@
 //  Created by Keith Smith on 11/20/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Design a class named Employee. The class should keep the following information:
+//
+//  * Employee name
+//  * Employee number
+//  * Hire date
+//
+//  Write one or more constructors, and the appropriate accessor and mutator functions,
+//  for the class.
+//
+//  Next write a class named ProductionWorker that is derived from the Employee class.
+//  The ProductionWorker class should have member variables to hold the following
+//  information:
+//
+//  * Shift (an integer)
+//  * Hourly pay rate (a double)
+//
+//  The workday is divided into two shifts: day and night. The shift variable will hold an
+//  integer value representing the shift that the employee works. The day shift is shift 1,
+//  and the night shift is shift 2. Write one or more constructors, and the appropriate
+//  accessor and mutator functions, for the class. Demonstrate the classes by writing a program
+//  that uses a ProductionWorker object.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
     return 0;
 }
